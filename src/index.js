4790d879bd4e1c26925bc7a61f6871966a05a92e
Mohamed Aachour <aachourmohameddeveloper@gmail.com>
2023-12-23 07:54:46 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,24 +1,4 @@
-import './style.css';
-import blockContainer from './modules/blockContainer.js';
+import "./style.css";
+import blockContainer from "./modules/blockContainer.js";
 
 blockContainer();
-
-// Toggle dark/light mode
-const sunIcon = document.querySelector('.sun');
-const moonIcon = document.querySelector('.moon');
-
-const darkMode = () => {
-  document.querySelector('html').style.filter = 'invert(0) hue-rotate(0deg)';
-  sunIcon.style.display = 'none';
-  moonIcon.style.display = 'block';
-};
-
-const lightMode = () => {
-  document.querySelector('html').style.filter = 'invert(0) hue-rotate(0deg)';
-  document.querySelector('html').style.filter = 'invert(1) hue-rotate(180deg)';
-  sunIcon.style.display = 'block';
-  moonIcon.style.display = 'none';
-};
-
-sunIcon.addEventListener('click', darkMode);
-moonIcon.addEventListener('click', lightMode);
